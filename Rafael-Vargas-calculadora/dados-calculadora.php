4173cc35a5afc1b140b6851ceef194e55b533b1e
RafaelVargas29 <rafaelrgsv@gmail.com>
2021-04-13 19:23:39 -0300

    Calculadora versão 2
    Adicionei algumas validações de dados no código.

--- Rafael-Vargas-calculadora/dados-calculadora.php
+++ Rafael-Vargas-calculadora/dados-calculadora.php
@@ -3,48 +3,75 @@
     $valorb = floatval($_POST['b']);
     $op = $_POST['operacao'];
     
-    if($valora == "" xor $valorb == ""){
+    
+    switch($op) {
+        case "soma":
+            $result = $valora + $valorb;
+             echo "O resultado da soma é: $result";
+        break;
+        case "sub":
+            $result = $valora - $valorb;
+             echo "O resultado da subtração é: $result";
+        break;
+        case "mult":
+            $result = $valora * $valorb;
+            echo "O resultado da multiplição é: $result";
+        break;
+        case "divisao":
+            $result = $valora / $valorb;
+            echo "O resultado da divisão é: $result";
+        break;
 
-        echo "Você deixou um campo em branco. Preencha-o para que o cálculo seja feito!";
-    }
-    else{
-        switch($op) {
-            case "soma":
-                $result = $valora + $valorb;
-                echo "O resultado da soma é: $result";
-            break;
-            case "sub":
-                $result = $valora - $valorb;
-                echo "O resultado da subtração é: $result";
-            break;
-            case "mult":
-                $result = $valora * $valorb;
-                echo "O resultado da multiplição é: $result";
-            break;
-            case "divisao":
-                $result = $valora / $valorb;
-                echo "O resultado da divisão é: $result";
+        case "porcenta";
+            $result = ($valora/100) * $valorb;
+            echo "$valora% de $valorb é: $result";
+        break;
+
+        case "aoquad";
+            if($valora == ""){
+                echo "Você dexou o campo <em>--Digite A--</em> em branco. Por favor, siga as instruções para uso da calculadora!";
+            }  
+            else{
+                if($valorb != ""){
+                    echo "Você preencheu o campo <em>--Digite B--</em>, por favor, siga as instruções para uso da calculadora!";
+                }
+                else{
+                    $result = pow($valora, 2);
+                    echo "O resultado de $valora<sup>2</sup> é: $result";  
+                }
+            } 
             break;
 
-            case "porcenta";
-                $result = ($valora/100) * $valorb;
-                echo "$valora% de $valorb é: $result";
-            break;
+        case "sobx";
+            if($valorb == ""){
+                echo "Você deixou o campo <em>--DIGITE B--</em> em branco. Por favor, siga as instruções para uso da calculadora!";
+            }
+            else{
+                if($valora != "")
+                {
+                    echo "Você preencheu o campo <em>--Digite A--</em>, por favor, siga as instruções para uso da calculadora!";
+                }
+                else{
+                    $result = 1/$valorb;
+                    echo "O resultado de 1 sobre $valorb é: $result";
+                }
+            }
+        break;
 
-            case "aoquad";
-                $result = pow($valora, 2);
-                echo "O resultado de $valora<sup>2</sup> é: $result";
-            break;
-
-            case "sobx";
-                $result = 1/$valorb;
-                echo "O resultado de 1 sobre $valorb é: $result";
-            break;
-
-            case "raizquad";
+        case "raizquad";
+        if($valora == ""){
+            echo "Você dexou o campo <em>--Digite A--</em> em branco. Por favor, siga as instruções para uso da calculadora!";
+        }  
+        else{
+            if($valorb != ""){
+                echo "Você preencheu o campo <em>--Digite B--</em>, por favor, siga as instruções para uso da calculadora!";
+            }
+            else{
                 $result = sqrt($valora);
-                echo "A Raiz quadrada de $valora é: $result";
-            break;
+                echo "A Raiz quadrada de $valora é: $result"; 
+            }
         }
+        break;
     }
+
 ?>
