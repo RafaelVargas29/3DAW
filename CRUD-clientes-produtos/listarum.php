31c41ddfcae83a9d3f4071a764e2e8919d5e7414
RafaelVargas29 <rafaelrgsv@gmail.com>
2021-05-17 22:58:25 -0300

    Update listarum.php

--- CRUD-clientes-produtos/listarum.php
+++ CRUD-clientes-produtos/listarum.php
@@ -35,10 +35,10 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>CRUD Clientes/Produtos | Listar todos</title>
+        <title>CRUD Clientes/Produtos | Listar um</title>
     </head>
     <body>
-        <h1>LISTA DE CLIENTES</h1>
+        <h1>CLIENTE</h1>
 
         <table style="border: 1px solid black";>
             <tr>
