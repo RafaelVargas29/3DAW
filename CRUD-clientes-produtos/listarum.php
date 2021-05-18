4e31ead253332d1b0ae00f66fee0360b8562d419
RafaelVargas29 <rafaelrgsv@gmail.com>
2021-05-18 17:23:07 -0300

    Update listarum.php
    atualização visual em "listaum.php"

--- CRUD-clientes-produtos/listarum.php
+++ CRUD-clientes-produtos/listarum.php
@@ -4,13 +4,55 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilo.css">
+    <style>
+        section{
+                position:relative;
+                top:90px;
+                border: 1px solid green;
+                text-align: center;
+                margin-top: 20px;
+        }
+        form{
+            border: 1px solid green;
+            width: 300px;
+            margin-left: 500px;
+            margin-bottom: 15px;
+            padding: 15px;
+        }
+        table{
+            border: 1px solid green;
+            margin-left: 300px;
+            margin-bottom: 15px;
+            padding: 15px;
+        }
+    </style>
 </head>
 <body>
-    <h1>Digite o nome do cliente</h1>
-    <form action="listarum.php" method="POST">
-        NOME:<br><input type="text" name="nome"><br><br>
-        <input type="submit" value="Listar">
-    </form>
+        <header>
+            <h1> Listar um| CRUD - Clientes</h1>
+        </header>
+        
+        <nav>
+            <a href="index.html"><button>Página inicial</button></a>
+            <a href="listartodos.php"><button>Listar todos os clientes</button></a>
+            <a href="excluir.php"><button>Excluir cliente</button></a>
+            <a href="editar.php"><button>Editar cliente</button></a>
+            <a href="criar.php"><button>Criar cliente</button></a>
+        </nav>
+        
+        <section>
+            <h3>Digite o nome do cliente</h3>
+            <form action="listarum.php" method="POST">
+                NOME:<br><input type="text" name="nome"><br><br>
+                <input type="submit" value="Listar">
+            </form>
+        </section>
+
+        <footer>
+            <h3>Rafael Vargas - Matricula: 1820478300010</h3>   
+        </footer>
+        
 </body>
 </html>
 
@@ -22,9 +64,7 @@
     if($nome != ""){
         $consulta = "SELECT * FROM `cliente` WHERE `nome` = '$nome'";
     }
-    else{
-        echo "O campo nome não pode estar vazio!";
-    }
+
 
     $con = $conn->query($consulta) or die ($conn->error);
 ?>
@@ -38,31 +78,32 @@
         <title>CRUD Clientes/Produtos | Listar um</title>
     </head>
     <body>
-        <h1>CLIENTE</h1>
-
-        <table style="border: 1px solid black";>
-            <tr>
-            <td style="border: 1px solid black";>ID</td>
-                <td style="border: 1px solid black";>NOME</td>
-                <td style="border: 1px solid black";>CPF</td>
-                <td style="border: 1px solid black";>ENDEREÇO</td>
-                <td style="border: 1px solid black";>CEP</td>
-                <td style="border: 1px solid black";>CIDADE</td>
-                <td style="border: 1px solid black";>ESTADO</td>
-            </tr>
-            
-            <?php
-                while($linha = $con->fetch_array()){ ?>
-            <tr>
-                <td style="border: 1px solid black";><?php echo $linha ["id"];?></td>
-                <td style="border: 1px solid black";><?php echo $linha ["nome"];?></td>
-                <td style="border: 1px solid black";><?php echo $linha ["cpf"];?></td>
-                <td style="border: 1px solid black";><?php echo $linha["endereco"];?></td>
-                <td style="border: 1px solid black";><?php echo $linha ["cep"];?></td>
-                <td style="border: 1px solid black";><?php echo $linha ["cidade"];?></td>
-                <td style="border: 1px solid black";><?php echo $linha ["estado"];?></td>  
-            </tr>
-            <?php }?>
-        </table>
+        <section>
+            <h4>CLIENTE</h4>
+            <table style="border: 1px solid green";>
+                <tr>
+                <td style="border: 1px solid green";>ID</td>
+                    <td style="border: 1px solid green";>NOME</td>
+                    <td style="border: 1px solid green";>CPF</td>
+                    <td style="border: 1px solid green";>ENDEREÇO</td>
+                    <td style="border: 1px solid green";>CEP</td>
+                    <td style="border: 1px solid green";>CIDADE</td>
+                    <td style="border: 1px solid green";>ESTADO</td>
+                </tr>
+                
+                <?php
+                    while($linha = $con->fetch_array()){ ?>
+                <tr>
+                    <td style="border: 1px solid green";><?php echo $linha ["id"];?></td>
+                    <td style="border: 1px solid green";><?php echo $linha ["nome"];?></td>
+                    <td style="border: 1px solid green";><?php echo $linha ["cpf"];?></td>
+                    <td style="border: 1px solid green";><?php echo $linha["endereco"];?></td>
+                    <td style="border: 1px solid green";><?php echo $linha ["cep"];?></td>
+                    <td style="border: 1px solid green";><?php echo $linha ["cidade"];?></td>
+                    <td style="border: 1px solid green";><?php echo $linha ["estado"];?></td>  
+                </tr>
+                <?php }?>
+            </table>
+        </section>
     </body>
 </html>
