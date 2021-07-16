96c1d37c9e327a25a0d7877bbb0aeb314984aa8b
RafaelVargas29 <79914727+RafaelVargas29@users.noreply.github.com>
2021-07-16 17:43:31 -0300

    implementando "server.php"

--- av2-crud-js-php/server.php
+++ av2-crud-js-php/server.php
@@ -1,8 +1,22 @@
 <?php
     include_once("conexaoprod.php");
+    
+   $codbarra = $_GET["codbarra"];
+   $nome = $_GET["nome"];
+   $fabri = $_GET["fabricante"];
+   $cate = $_GET["categoria"];
+   $tipo = $_GET["tipo"];
+   $preco = $_GET["preco"];
+   $quant = $_GET["quantestoque"];
+   $peso = $_GET["peso"];
+   $descr = $_GET["descricao"];
+   $data = $_GET["data"];
+   $ativo = $_GET["ativo"];
+   $varcase = $_GET["varcase"];
 
-    $consulta = "INSERT INTO `cliente`(`nome`, `cpf`, `endereco`, `cep`, `cidade`, `estado`) VALUES 
-    ('$nome','$cpf','$endereco','$cep','$cidade','$estado')";
+    $consulta = "INSERT INTO `produto`(`codbarra`, `nome`, `fabricante`, `categoria`, `tipo`, `precovenda`
+    , `quantestoque`, `peso`, `descricao`, `dataincl`, `ativo`) VALUES ('$codbarra','$nome','$fabri','$cate',
+    '$tipo','$preco','$quant','$peso','$descr','$data','$ativo')";
 
     $con = $conn->query($consulta) or die ($conn->error);
 ?>
