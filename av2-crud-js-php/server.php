6c173849cabd53c1f7f8fc6d886c9796c4b152b8
RafaelVargas29 <rafaelrgsv@gmail.com>
2021-07-17 16:55:34 -0300

    update
    atualização no cod "server.php" e "incluir.html"

--- av2-crud-js-php/server.php
+++ av2-crud-js-php/server.php
@@ -1,22 +1,23 @@
 <?php
     include_once("conexaoprod.php");
     
-    $codbarra = $_GET["codbarra"];
-    $nome = $_GET["nome"];
-    $fabri = $_GET["fabricante"];
-    $cate = $_GET["categoria"];
-    $tipo = $_GET["tipo"];
-    $preco = $_GET["preco"];
-    $quant = $_GET["quantestoque"];
-    $peso = $_GET["peso"];
-    $descr = $_GET["descricao"];
-    $data = $_GET["data"];
-    $ativo = $_GET["ativo"];
-    $varcase = $_GET["varcase"];
-
-    $consulta = "INSERT INTO `produto`(`codbarra`, `nome`, `fabricante`, `categoria`, `tipo`, `precovenda`
+    $codbarra = $_GET["icodbarra"];
+    $nome = $_GET["inome"];
+    $fabri = $_GET["ifabricante"];
+    $cate = $_GET["icategoria"];
+    $tipo = $_GET["itipo"];
+    $preco = $_GET["ipreco"];
+    $quant = $_GET["iquantestoque"];
+    $peso = $_GET["ipeso"];
+    $descr = $_GET["idescricao"];
+    $data = $_GET["idata"];
+    //$ativo = $_GET["iativo"];
+    //$varcase = $_GET["ivarcase"];
+    
+    $consulta = "INSERT INTO `produtos`(`codbarra`, `nome`, `fabricante`, `categoria`, `tipo`, `precovenda`
     , `quantestoque`, `peso`, `descricao`, `dataincl`, `ativo`) VALUES ('$codbarra','$nome','$fabri','$cate',
     '$tipo','$preco','$quant','$peso','$descr','$data','$ativo')";
 
     $con = $conn->query($consulta) or die ($conn->error);
+    
 ?>
