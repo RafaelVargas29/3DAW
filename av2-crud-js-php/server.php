2c0e933249fffd1fddf80c825687880e72fd4f92
RafaelVargas29 <79914727+RafaelVargas29@users.noreply.github.com>
2021-07-16 21:00:19 -0300

    update "incluir.html"

--- av2-crud-js-php/server.php
+++ av2-crud-js-php/server.php
@@ -1,18 +1,18 @@
 <?php
     include_once("conexaoprod.php");
     
-   $codbarra = $_GET["codbarra"];
-   $nome = $_GET["nome"];
-   $fabri = $_GET["fabricante"];
-   $cate = $_GET["categoria"];
-   $tipo = $_GET["tipo"];
-   $preco = $_GET["preco"];
-   $quant = $_GET["quantestoque"];
-   $peso = $_GET["peso"];
-   $descr = $_GET["descricao"];
-   $data = $_GET["data"];
-   $ativo = $_GET["ativo"];
-   $varcase = $_GET["varcase"];
+    $codbarra = $_GET["codbarra"];
+    $nome = $_GET["nome"];
+    $fabri = $_GET["fabricante"];
+    $cate = $_GET["categoria"];
+    $tipo = $_GET["tipo"];
+    $preco = $_GET["preco"];
+    $quant = $_GET["quantestoque"];
+    $peso = $_GET["peso"];
+    $descr = $_GET["descricao"];
+    $data = $_GET["data"];
+    $ativo = $_GET["ativo"];
+    $varcase = $_GET["varcase"];
 
     $consulta = "INSERT INTO `produto`(`codbarra`, `nome`, `fabricante`, `categoria`, `tipo`, `precovenda`
     , `quantestoque`, `peso`, `descricao`, `dataincl`, `ativo`) VALUES ('$codbarra','$nome','$fabri','$cate',
