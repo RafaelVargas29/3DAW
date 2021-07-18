b1b4aca438062d65d3304b8e66c4266e17bb132c
RafaelVargas29 <rafaelrgsv@gmail.com>
2021-07-18 11:02:13 -0300

    renomeando
    "server.php" passa a ser chamado de "incluir.php"

--- av2-crud-js-php/incluir.php
+++ av2-crud-js-php/incluir.php
@@ -12,14 +12,9 @@
     $descr = $_GET["idescricao"];
     $data = $_GET["idata"];
     $ativo = $_GET["iativo"];
-    //$varcase = $_GET["ivarcase"];
     
     $consulta = "INSERT INTO `produtos`(`codbarra`, `nome`, `fabricante`, `categoria`, `tipo`, `precovenda`, `quantestoque`,`peso`,`descricao`,`dataincl`,`ativo`) VALUES ('$codbarra','$nome','$fabri','$cate','$tipo','$preco','$quant','$peso','$descr','$data','$ativo')";
-/*
-    $consulta = "INSERT INTO `produtos`(`codbarra`, `nome`, `fabricante`, `categoria`, `tipo`, `precovenda`
-    , `quantestoque`, `peso`, `descricao`, `dataincl`, `ativo`) VALUES ('$codbarra','$nome','$fabri','$cate',
-    '$tipo','$preco','$quant','$peso','$descr','$data','$ativo')";
-*/
+
     $con = $conn->query($consulta) or die ($conn->error);
     
 ?>
