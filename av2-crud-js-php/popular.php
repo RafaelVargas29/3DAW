cf61d9744900f2c425c62aad1ecade0e40f0a441
RafaelVargas29 <rafaelrgsv@gmail.com>
2021-07-18 18:31:52 -0300

    criando "popula.php"
    código para popular um select

---
+++ av2-crud-js-php/popular.php
@@ -0,0 +1,20 @@
+<?php
+    include_once("conexaoprod.php");
+    
+    $consulta = "SELECT * FROM categoria";
+
+    $con = $conn->query($consulta) or die ($conn->error);
+
+    $i = 0;
+
+    $arrCate[] = array();
+
+    while($linha = $con->fetch_assoc()){
+
+        $arrCate[$i] = utf8_decode($linha["nome"]);
+        $i++;
+    }
+
+    echo json_encode($arrCate);
+
+?>
