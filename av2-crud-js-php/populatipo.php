55eda36bcf43d70246cafa7ff624d657bc8fca1d
RafaelVargas29 <rafaelrgsv@gmail.com>
2021-07-19 01:05:15 -0300

    create "populatipo.php"
    Funcionalidade "incluir" funcionando (sem validações)

---
+++ av2-crud-js-php/populatipo.php
@@ -0,0 +1,20 @@
+<?php
+    include_once("conexaoprod.php");
+    
+    $consulta = "SELECT * FROM tipo";
+
+    $con = $conn->query($consulta) or die ($conn->error);
+
+    $i = 0;
+
+    $arrTipo[] = array();
+
+    while($linha = $con->fetch_assoc()){
+
+        $arrTipo[$i] = utf8_decode($linha["nome"]);
+        $i++;
+    }
+
+    echo json_encode($arrTipo);
+
+?>
