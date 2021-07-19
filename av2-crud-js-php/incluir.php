f98bf421dd2a1e5e095f6803965e30af15d73f11
RafaelVargas29 <rafaelrgsv@gmail.com>
2021-07-19 20:04:21 -0300

    criando "alterar.html"

--- av2-crud-js-php/incluir.php
+++ av2-crud-js-php/incluir.php
@@ -2,23 +2,45 @@
     if ($_SERVER["REQUEST_METHOD"] == "GET"){
         include_once("conexaoprod.php");
     
-    $codbarra = $_GET["icodbarra"];
-    $nome = $_GET["inome"];
-    $fabri = $_GET["ifabricante"];
-    $cate = $_GET["icategoria"];
-    $tipo = $_GET["itipo"];
-    $preco = $_GET["ipreco"];
-    $quant = $_GET["iquantestoque"];
-    $peso = $_GET["ipeso"];
-    $descr = $_GET["idescricao"];
-    $data = $_GET["idata"];
-    $ativo = $_GET["iativo"];
+        $codbarra = $_GET["icodbarra"];
+        $nome = $_GET["inome"];
+        $fabri = $_GET["ifabricante"];
+        $cate = $_GET["icategoria"];
+        $tipo = $_GET["itipo"];
+        $preco = $_GET["ipreco"];
+        $quant = $_GET["iquantestoque"];
+        $peso = $_GET["ipeso"];
+        $descr = $_GET["idescricao"];
+        $data = $_GET["idata"];
+        $ativo = $_GET["iativo"];
+        $formInvalido = 0;
+        
+        function validaDadosAlpha($formDado) {
+            if ($formDado == "" or ctype_alpha($formDado)) {
+                $formInvalido = 1;
+            }
+        }
+        function validaDadosDigit($formDado) {
+            if ($formDado == "" or !ctype_digit($formDado)) {
+                $formInvalido = 1;
+            }
+        }
+        
+        validaDadosDigit($codbarra);
+        validaDadosAlpha($nome);
+        validaDadosAlpha($nome);
+        validaDadosAlpha($fabricante);
+        validaDadosAlpha($cate);
+        validaDadosDigit($preco);
+        validaDadosDigit($quant);
+        validaDadosDigit($peso);
+        validaDadosAlpha($descr);
 
-
-
-    $consulta = "INSERT INTO `produtos`(`codbarra`, `nome`, `fabricante`, `categoria`, `tipo`, `precovenda`, `quantestoque`,`peso`,`descricao`,`dataincl`,`ativo`) VALUES ('$codbarra','$nome','$fabri','$cate','$tipo','$preco','$quant','$peso','$descr','$data','$ativo')";
-    
-    $con = $conn->query($consulta) or die ($conn->error);
+        if($formInvalido == 0){
+            $consulta = "INSERT INTO `produtos`(`codbarra`, `nome`, `fabricante`, `categoria`, `tipo`, `precovenda`, `quantestoque`,`peso`,`descricao`,`dataincl`,`ativo`) VALUES ('$codbarra','$nome','$fabri','$cate','$tipo','$preco','$quant','$peso','$descr','$data','$ativo')";
+        
+            $con = $conn->query($consulta) or die ($conn->error);
+        }
     }
     
 ?>
